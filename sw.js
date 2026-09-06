6ee767959acdecd4271582c965472c2f787721b3
Fabian-Robert Stöter <fabian@audioshake.ai>
2026-09-06 10:01:08 +0200

    Move submit button beside writing pad

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "dr-stoeter-gehirnjogging-";
-const CACHE_NAME = `${CACHE_PREFIX}v8`;
+const CACHE_NAME = `${CACHE_PREFIX}v10`;
 const APP_ASSETS = [
   "./",
   "./index.html",
