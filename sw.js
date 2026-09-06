7c5dbac51f26f0e9dab8e7a24cafdd8873a0490a
Fabian-Robert Stöter <fabian@audioshake.ai>
2026-09-06 16:44:57 +0200

    Remove home handwriting explainer

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "dr-stoeter-gehirnjogging-";
-const CACHE_NAME = `${CACHE_PREFIX}v30`;
+const CACHE_NAME = `${CACHE_PREFIX}v31`;
 const APP_ASSETS = [
   "./",
   "./index.html",
