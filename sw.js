b031b6fd9fef3ab26efce5072b41c612dae8e214
Fabian-Robert Stöter <fabian@audioshake.ai>
2026-09-06 18:28:38 +0200

    Use full safe area in iOS standalone app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "dr-stoeter-gehirnjogging-";
-const CACHE_NAME = `${CACHE_PREFIX}v42`;
+const CACHE_NAME = `${CACHE_PREFIX}v43`;
 const APP_ASSETS = [
   "./",
   "./index.html",
