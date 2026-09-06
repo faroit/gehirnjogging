63b3e75a1640a873319930eba199657c57fc60b2
Fabian-Robert Stöter <fabian@audioshake.ai>
2026-09-06 22:22:57 +0200

    Limit fullscreen control to desktop

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "dr-stoeter-gehirnjogging-";
-const CACHE_NAME = `${CACHE_PREFIX}v45`;
+const CACHE_NAME = `${CACHE_PREFIX}v46`;
 const APP_ASSETS = [
   "./",
   "./index.html",
