be2f3c193cc5c582579544bc3d4ec30a605848ca
Fabian-Robert Stöter <fabian@audioshake.ai>
2026-09-06 16:43:37 +0200

    Adopt editorial serif typography

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "dr-stoeter-gehirnjogging-";
-const CACHE_NAME = `${CACHE_PREFIX}v29`;
+const CACHE_NAME = `${CACHE_PREFIX}v30`;
 const APP_ASSETS = [
   "./",
   "./index.html",
