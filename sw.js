909e4b46c83778f79d117f1d20d377a5c4889c92
Fabian-Robert Stöter <fabian@audioshake.ai>
2026-09-06 15:29:37 +0200

    Refine keypad color hierarchy

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "dr-stoeter-gehirnjogging-";
-const CACHE_NAME = `${CACHE_PREFIX}v16`;
+const CACHE_NAME = `${CACHE_PREFIX}v17`;
 const APP_ASSETS = [
   "./",
   "./index.html",
