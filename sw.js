4e25067121f4725d40d67f4c55ab3413c79b178a
Fabian-Robert Stöter <fabian@audioshake.ai>
2026-09-06 16:30:29 +0200

    Remove home card number

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "dr-stoeter-gehirnjogging-";
-const CACHE_NAME = `${CACHE_PREFIX}v25`;
+const CACHE_NAME = `${CACHE_PREFIX}v26`;
 const APP_ASSETS = [
   "./",
   "./index.html",
