51202b244f88628f002dfc10c94c11dca319fd0e
Fabian-Robert Stöter <fabian@audioshake.ai>
2026-09-07 09:52:40 +0200

    Add animated game preview to home

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "dr-stoeter-gehirnjogging-";
-const CACHE_NAME = `${CACHE_PREFIX}v55`;
+const CACHE_NAME = `${CACHE_PREFIX}v56`;
 const APP_ASSETS = [
   "./",
   "./index.html",
