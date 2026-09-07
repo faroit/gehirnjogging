d913329b5a1f7904c0bc0e3b5bc975b30a47c45d
Fabian-Robert Stöter <fabian@audioshake.ai>
2026-09-07 11:15:46 +0200

    Expose app canvas through Safari controls

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "dr-stoeter-gehirnjogging-";
-const CACHE_NAME = `${CACHE_PREFIX}v64`;
+const CACHE_NAME = `${CACHE_PREFIX}v65`;
 const APP_ASSETS = [
   "./",
   "./index.html",
