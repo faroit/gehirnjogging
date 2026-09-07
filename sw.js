78d3d35a43d9da422fa38cf99332cb95e65c42ee
Fabian-Robert Stöter <fabian@audioshake.ai>
2026-09-07 09:50:48 +0200

    Remove game hint copy

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "dr-stoeter-gehirnjogging-";
-const CACHE_NAME = `${CACHE_PREFIX}v54`;
+const CACHE_NAME = `${CACHE_PREFIX}v55`;
 const APP_ASSETS = [
   "./",
   "./index.html",
