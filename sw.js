131849cebbc24031a7010ef07feb12209c4292e1
Fabian-Robert Stöter <fabian@audioshake.ai>
2026-09-07 09:41:40 +0200

    Refine settings control icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "dr-stoeter-gehirnjogging-";
-const CACHE_NAME = `${CACHE_PREFIX}v50`;
+const CACHE_NAME = `${CACHE_PREFIX}v51`;
 const APP_ASSETS = [
   "./",
   "./index.html",
